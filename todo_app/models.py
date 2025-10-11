14275a5116ea14b7b2295ab129cdc39a30541ff5
ka7me <144337401+ka7me@users.noreply.github.com>
2025-10-11 13:00:07 +0900

    Update models.py

--- todo_app/models.py
+++ todo_app/models.py
@@ -16,9 +16,18 @@ class Todo(db.Model):
     done = db.Column(db.Boolean, default=False)
     date = db.Column(db.Date, default=date.today)
 
+    def set_attack_power(self):
+        if self.difficulty == 1:
+            self.attack_power = 10
+        elif self.difficulty == 2:
+            self.attack_power = 20
+        elif self.difficulty == 3:
+            self.attack_power = 30
+        else:
+            self.attack_power = 0
+
 class Monster(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), default="モンスター")
     hp = db.Column(db.Integer, default=0)
     date = db.Column(db.Date, default=date.today)
-
