17db41086ebe990f29cf00d6c2b1b1219bcd00c4
ka7me <144337401+ka7me@users.noreply.github.com>
2025-10-11 11:43:39 +0900

    Update models.py

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,1 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+
+db = SQLAlchemy()
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), default="プレイヤー")
+    level = db.Column(db.Integer, default=1)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    difficulty = db.Column(db.String(20))
+    attack_power = db.Column(db.Integer, default=0)
+    done = db.Column(db.Boolean, default=False)
+    date = db.Column(db.Date, default=date.today)
+
+class Monster(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), default="モンスター")
+    hp = db.Column(db.Integer, default=0)
+    date = db.Column(db.Date, default=date.today)
 
